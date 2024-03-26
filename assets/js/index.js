82e30cc92f498ca23ff561c0bdac1fdb2370d7f1
Karasya <nikita.carasevitch@yandex.by>
2024-03-26 20:46:43 +0300

    Fixed-links

--- assets/js/index.js
+++ assets/js/index.js
@@ -224,3 +224,15 @@ function Result(){
 
 
 ///////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+let LINKS = document.querySelector('.messangers__links-btn')
+let MENU = document.querySelector('.messangers__links-menu')
+LINKS.addEventListener('click',((e)=>{
+    if(e.target.closest('.messangers__links-btn')){
+        MENU.classList.toggle('active')
+    }
+}))
