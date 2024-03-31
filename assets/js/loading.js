feade4f07e465b5e1a5917fb26aafa8602dacd50
Karasya <nikita.carasevitch@yandex.by>
2024-03-31 18:42:47 +0300

    /

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,6 +1,6 @@
 
-// let LOAD = document.querySelector('.loading')
-// window.addEventListener('load',((event)=>{
-//     LOAD.classList.add('active')
-//     BODY.classList.remove('activebody')
-// }))
+let LOAD = document.querySelector('.loading')
+window.addEventListener('load',((event)=>{
+    LOAD.classList.add('active')
+    BODY.classList.remove('activebody')
+}))
