ecb12ed7fdc5a81a6bcaef4c56181fddaf9ff182
Karasya <nikita.carasevitch@yandex.by>
2024-04-09 20:29:25 +0300

    reviews slider

--- assets/js/asyncreviews.js
+++ assets/js/asyncreviews.js
@@ -7,3 +7,20 @@
 function videoUrl(url) {
     document.querySelector(".reviews__videos-active").src = url;
 }
+
+
+$('.reviews__cards-wrapper').slick({
+  infinite: true,
+  dots:false,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    }]
+});
