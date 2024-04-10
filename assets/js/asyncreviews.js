8408ee6461e562e99a2994df7d0acde6a4a13758
Karasya <nikita.carasevitch@yandex.by>
2024-04-10 19:34:52 +0300

    get data reviews

--- assets/js/asyncreviews.js
+++ assets/js/asyncreviews.js
@@ -2,9 +2,11 @@
 
 const REVIEWSFORM = document.querySelector('.reviews-form')
 const CLOSEREVIEWSBTN = document.querySelector('.reviews-form__close')
-const REVIEWSFORMBTN = document.querySelector('.reviews__cards-slader-btn')
+const REVIEWSCARDSLIDERBTN = document.querySelector('.reviews__cards-slader-btn')
+const REVIEWSFORMBTN = document.querySelector('.reviews-form')
 
-REVIEWSFORMBTN.addEventListener('click',(event)=>{
+
+REVIEWSCARDSLIDERBTN.addEventListener('click',(event)=>{
   if(event.target.closest('.reviews__cards-slader-btn')){
     REVIEWSFORM.classList.add('active')
     BODY.classList.add('activebody')
@@ -19,10 +21,56 @@ CLOSEREVIEWSBTN.addEventListener('click',((event)=>{
 
 }))
 
+REVIEWSFORM.addEventListener('submit',((event)=>{SendReviews(event)}))
 
 
 
+function SendReviews(event){
+  event.preventDefault()
+  let formdata = new FormData(event.target)
+  Object.fromEntries(formdata)
+  POPUPMENU.classList.remove('active')
+  BODY.classList.remove('activebody')
+  event.target.reset()
+  let obj ={}
 
+  let currentdate = new Date();
+  let datetime = currentdate.getDate() + "."
+                + (currentdate.getMonth()+1)  + "." 
+                + currentdate.getFullYear()
+  
+  for (var pair of formdata.entries()) {
+      obj[pair[0]] = pair[1]  
+  }
+
+  let newreview = {
+    username:obj.name,
+    telephone:obj.telephone,
+    email:obj.email,
+    city:obj.city,
+    review:obj.review,
+    date:datetime,
+
+  };
+  
+  fetch('https://660996190f324a9a28837e41.mockapi.io/Comments', {
+  method: 'POST',
+  headers: {'content-type':'application/json'},
+  body: JSON.stringify(newreview)
+}).then(res => {
+  if (res.ok) {
+      return window.location.reload();
+  }
+})}
+
+
+async function GetReviews(){
+  let response = await fetch('https://660996190f324a9a28837e41.mockapi.io/Comments')
+  let data = await response.json()
+  data.forEach(element => {
+    console.log(element)
+  }); 
+}
 
 
 
--- assets/js/loading.js
+++ assets/js/loading.js
@@ -3,4 +3,5 @@ let LOAD = document.querySelector('.loading')
 window.addEventListener('load',((event)=>{
     LOAD.classList.add('active')
     BODY.classList.remove('activebody')
+    GetReviews()
 }))
